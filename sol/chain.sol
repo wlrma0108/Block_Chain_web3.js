9a35092c9da0fae2dea71917d34b50d2cf393260
wlrma0108 <dlwlrma0108@gmail.com>
2022-09-10 23:50:19 +0900

    commit

---
+++ sol/chain.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.15;
+
+contract ExampleViewPure {
+
+    uint public myStorageVariable;
+
+    function setMyStorageVariable(uint _newVar) public returns(uint) {
+        myStorageVariable = _newVar;
+        return _newVar;
+    }
+
+
+}
